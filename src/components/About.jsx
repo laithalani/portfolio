b8c2be459db2e50be58729eb5eede8744b56c119
Laith Alani <laithbassim@yahoo.com>
2023-01-05 21:58:21 -0600

    add color gradient

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500 text-white'>
                     About
                 </p>
             </div>
-            <p className='text-xl mt-20'>
+            <p className='text-xl mt-20 text-white'>
             I love to learn new techs and expand my knowledge. I have done a few web development and there are a lot of parts to it. The part that I love is that I can build something and I can see it immediately. I can show it to people and they can interact with it. 
             </p>
             <br />
-            <p className='text-xl'>
+            <p className='text-xl text-white'>
                 Many people love being web developer because they can extend their creativity and passion on developing websites, and I’m one of them.
             </p>
         </div>
